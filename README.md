7bd7fadde53ef76a730a2c36b71603684adc707b
Adi Pradosh <adi.pradosh@gmail.com>
2015-11-28 17:16:47 -0500

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # science-project-database
 Repo for science project database
+
+# Setup PostgreSQL
+
+- Make sure postgres 9.4 or higher is installed
+
+- Create standard user
+
+
+# Setup NodeJS
+
+
+
+# REST APIs
+
+Here are the REST APIs supported by the Node-PostgreS application
+
+## USER APIs
+POST /api/v1/user
+   	- Creates user
+
+DELETE /api/v1/user/{email}
+	- Deletes user
+
+GET /api/v1/user/{email}
+	- Gets the user info
+
+
+## Questions Reports APIs
+POST /api/v1/question/info
+	- Posts a question
---
+++ db/postgres.sql
@@ -0,0 +1,83 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: postgres; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON DATABASE postgres IS 'default administrative connection database';
+
+
+--
+-- Name: test; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA test;
+
+
+ALTER SCHEMA test OWNER TO postgres;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: user_data; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE user_data (
+    data jsonb
+);
+
+
+ALTER TABLE user_data OWNER TO postgres;
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+GRANT USAGE ON SCHEMA public TO report_db;
+
+
+--
+-- Name: user_data; Type: ACL; Schema: public; Owner: postgres
+--
+
+REVOKE ALL ON TABLE user_data FROM PUBLIC;
+REVOKE ALL ON TABLE user_data FROM postgres;
+GRANT ALL ON TABLE user_data TO postgres;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE user_data TO report_db;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
